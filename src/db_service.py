7340e4a40e22c6f0ea11325965f76c3378a54c5e
fepego-penag <cf.pena1138@uniandes.edu.co>
2023-02-16 01:04:59 -0500

    adjusting test and coverture

--- src/db_service.py
+++ src/db_service.py
@@ -3,18 +3,6 @@ from boto3.dynamodb.conditions import Key
 
 import app
 
-
-def insert_item(body):
-
-    try:
-        client = boto3.resource("dynamodb")
-        table = client.Table(app.ENV_TABLE_NAME)
-        table.put_item(Item=body)
-        return True
-    except Exception as e:
-        raise RuntimeError('cannot persist on db cause: ' + str(e))
-
-
 def get_item(key, value):
     client = boto3.resource('dynamodb')
     try:
--- test/test_app.py
+++ test/test_app.py
@@ -41,16 +41,50 @@ def data_table(aws_credentials):
 
         yield TABLE_NAME
 
-def test_givenMalformedRequestOnRequestThenReturnError412(lambda_environment, data_table):
+@pytest.fixture
+def load_table_mocks(data_table):
+    client = boto3.resource("dynamodb")
+    table = client.Table(app.ENV_TABLE_NAME)
+    result = table.put_item(Item={ 'doctor_id': '123', 'license_number': '1212', 'status': 'pending'})
+    print(str(result))
+def test_givenvalidRequestThenReturn200AndRegistry(lambda_environment, load_table_mocks):
 
     event = {
             "resource": "/doctor/{doctor_id}/license",
             "path": "/doctor/license",
-            "httpMethod": "POST",
+            "httpMethod": "GET",
             "pathParameters": {
+                "doctor_id": "123"
             },
-            "body": "{\n    \"license_number\": \"234353\" \n}",
             "isBase64Encoded": False
+    }
+    lambdaResponse = app.handler(event, [])
+
+
+    assert lambdaResponse['statusCode'] == 200
+    assert lambdaResponse['body'] == '{"doctor_id": "123", "license_number": "1212", "status": "pending"}'
+
+def test_givenMalformedRequestOnRequestThenReturnError412(lambda_environment, load_table_mocks):
+    event = {
+        "resource": "/doctor/{doctor_id}/license",
+        "path": "/doctor/license",
+        "httpMethod": "GET",
+        "isBase64Encoded": False
+    }
+    lambdaResponse = app.handler(event, [])
+
+
+    assert lambdaResponse['statusCode'] == 500
+    assert lambdaResponse['body'] == '{"message": "cannot proceed with the request error: \'pathParameters\'"}'
+
+def test_givenMalformedRequestNoQueryParamOnRequestThenReturnError412(lambda_environment, load_table_mocks):
+    event = {
+        "resource": "/doctor/{doctor_id}/license",
+        "path": "/doctor/license",
+        "httpMethod": "GET",
+        "pathParameters": {
+        },
+        "isBase64Encoded": False
     }
     lambdaResponse = app.handler(event, [])
 
@@ -58,16 +92,32 @@ def test_givenMalformedRequestOnRequestThenReturnError412(lambda_environment, da
     assert lambdaResponse['statusCode'] == 412
     assert lambdaResponse['body'] == '{"message": "missing or malformed request body"}'
 
-def test_givenValidRequestAndDBFailureThenReturn500(lambda_environment):
+def test_givenvalidRequestForNonExistingREgistryThenReturn200AndEmptyResponse(lambda_environment, load_table_mocks):
 
     event = {
             "resource": "/doctor/{doctor_id}/license",
             "path": "/doctor/license",
-            "httpMethod": "POST",
+            "httpMethod": "GET",
             "pathParameters": {
-                "doctor_id": "123"
+                "doctor_id": "12344"
             },
-            "body": "{\n    \"license_number\": 234353\n}",
+            "isBase64Encoded": False
+    }
+    lambdaResponse = app.handler(event, [])
+
+
+    assert lambdaResponse['statusCode'] == 200
+    assert lambdaResponse['body'] == '[]'
+
+def test_givenvalidRequestWithErrorOnDynamoThenReturn500(lambda_environment):
+
+    event = {
+            "resource": "/doctor/{doctor_id}/license",
+            "path": "/doctor/license",
+            "httpMethod": "GET",
+            "pathParameters": {
+                "doctor_id": "12344"
+            },
             "isBase64Encoded": False
     }
     lambdaResponse = app.handler(event, [])
